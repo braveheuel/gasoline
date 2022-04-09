746b0611d09dd589ec3d3928da95c66d36ace9e4
Christoph Heuel <christoph@heuel-web.de>
2022-04-10 00:45:52 +0200

    Adapt to new timeout Version

--- sensor.py
+++ sensor.py
@@ -116,7 +116,7 @@ class GasolineData(object):
         resp = None
         try:
             websession = async_get_clientsession(self._hass)
-            with async_timeout.timeout(40, loop=self._hass.loop):
+            with async_timeout.timeout(40):
                 resp = yield from websession.get(station_url)
             if resp.status != 200:
                 _LOGGER.warning("Error fetching {} with response status {}!".format(station_url, resp.status))
